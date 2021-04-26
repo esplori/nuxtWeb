b3f4ae24eae9f6ea1e9f7f26c4104bde4d44a6d5
wilkwo <breakcastle@hotmail.com>
2021-04-26 22:00:00 +0800

    update pagesize to common components

--- pages/tbk/page/_id.vue
+++ pages/tbk/page/_id.vue
@@ -2,31 +2,7 @@
   <div class="container">
     <homeHeader></homeHeader>
     <div class="home-body">
-      <a
-        class="tbk-item"
-        v-for="(item, index) in list"
-        :key="index"
-        :href="'/tbk/detail/' + item.id"
-        target="_blank"
-      >
-        <div>
-          <a
-            ><img :src="item.pict_url" alt="" width="197px" height="197px"
-          /></a>
-          <a class="item-title">{{ item.title }}</a>
-          <div class="item-sale-info">
-            <div>
-              <a class="zk_final_price"
-                ><span class="rmbicon">¥</span>{{ item.zk_final_price }}</a
-              >
-              <a class="reserve_price"
-                ><span class="rmbicon">¥</span>{{ item.reserve_price }}</a
-              >
-            </div>
-            <div class="volume">月销{{ item.volume }}笔</div>
-          </div>
-        </div>
-      </a>
+      <goodsBody :list="list"></goodsBody>
     </div>
     <div style="text-align: center">
       <el-pagination
@@ -46,6 +22,7 @@ import { getShopListApi } from "../../api/index";
 export default {
   components: {
     homeHeader: () => import("../../../components/homeHeader.vue"),
+    goodsBody: () => import("../../../components/tbk/goodsBody.vue"),
   },
   data() {
     return {
@@ -53,17 +30,29 @@ export default {
     };
   },
   head() {
-    return {};
+    return {
+      title: "淘宝优乐购",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "淘宝优乐购",
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content: "淘宝优乐购",
+        },
+      ],
+    };
   },
   async asyncData({ $axios, route }) {
     // this.page = route.params.id || 1
-    let [res] = await Promise.all([
-      getShopListApi({ page: route.params.id || 1 }),
-    ]);
+    let [res] = await Promise.all([getShopListApi({})]);
     // console.log("res.data---------", res.data);
     return {
       list: res.data.result,
-      total: res.data.total || 90,
+      total: res.data.total || 80,
       page: parseInt(route.params.id || 1)
     };
   },
@@ -83,43 +72,6 @@ export default {
     margin: 10px auto;
     display: flex;
     flex-wrap: wrap;
-    .tbk-item:hover {
-      border: 1px solid #ff5000;
-    }
-    .tbk-item {
-      display: inline-block;
-      width: 235px;
-      padding: 20px;
-      border: 1px solid #f4f4f4;
-      cursor: pointer;
-      .item-title {
-        display: block;
-        height: 40px;
-        line-height: 20px;
-        overflow: hidden;
-        margin: 8px 0;
-        color: #666;
-        font-size: 14px;
-      }
-      .item-sale-info {
-        color: #999;
-        display: flex;
-        justify-content: space-between;
-        align-items: flex-end;
-        .zk_final_price {
-          color: #ff5000;
-          font-size: 22px;
-        }
-        .reserve_price {
-          color: #999;
-          font-size: 12px;
-          text-decoration: line-through;
-        }
-        .rmbicon {
-          font-size: 12px;
-        }
-      }
-    }
   }
 }
 </style>
